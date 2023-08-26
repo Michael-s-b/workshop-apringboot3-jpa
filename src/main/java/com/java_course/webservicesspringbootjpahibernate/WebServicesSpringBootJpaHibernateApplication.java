50f9c6ec571827ec915c3df82b07eb63f1e4384a
michael-s-b <michelsilva098@gmail.com>
2023-08-26 16:59:35 -0300

    -project created

---
+++ src/main/java/com/java_course/webservicesspringbootjpahibernate/WebServicesSpringBootJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.java_course.webservicesspringbootjpahibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicesSpringBootJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServicesSpringBootJpaHibernateApplication.class, args);
+	}
+
+}
