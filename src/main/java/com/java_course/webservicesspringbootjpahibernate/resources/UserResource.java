8c9f1e3127f0bc42819b370ced7e0aafae8d8201
michael-s-b <michelsilva098@gmail.com>
2023-08-27 14:48:26 -0300

    -Service layer, component registration

--- src/main/java/com/java_course/webservicesspringbootjpahibernate/resources/UserResource.java
+++ src/main/java/com/java_course/webservicesspringbootjpahibernate/resources/UserResource.java
@@ -1,18 +1,32 @@
 package com.java_course.webservicesspringbootjpahibernate.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.java_course.webservicesspringbootjpahibernate.entities.User;
+import com.java_course.webservicesspringbootjpahibernate.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+    @Autowired
+    private UserService UserService;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L, "Maria", "maria@gmail.com", "12345", "9999999");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> users = UserService.findAll();
+        return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = UserService.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
---
+++ src/main/java/com/java_course/webservicesspringbootjpahibernate/services/UserService.java
@@ -0,0 +1,27 @@
+package com.java_course.webservicesspringbootjpahibernate.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.java_course.webservicesspringbootjpahibernate.entities.User;
+
+import com.java_course.webservicesspringbootjpahibernate.repositories.UserRepository;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+
+}
