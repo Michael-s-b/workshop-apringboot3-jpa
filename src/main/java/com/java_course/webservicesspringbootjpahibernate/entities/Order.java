bb5b64f5577ab671f83c6eba5c8e8ce075180148
michael-s-b <michelsilva098@gmail.com>
2023-09-10 20:54:39 -0300

    Subtotal & Total methods

--- src/main/java/com/java_course/webservicesspringbootjpahibernate/entities/Order.java
+++ src/main/java/com/java_course/webservicesspringbootjpahibernate/entities/Order.java
@@ -96,6 +96,14 @@ public class Order implements Serializable {
         this.payment = payment;
     }
 
+    public Double getTotal() {
+        Double total = 0.0;
+        for (OrderItem item : items) {
+            total += item.getSubTotal();
+        }
+        return total;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == this)
