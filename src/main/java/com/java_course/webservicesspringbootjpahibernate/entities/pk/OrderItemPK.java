de280f9d03ae8ff21649acba341a1d3c27ef7343
michael-s-b <michelsilva098@gmail.com>
2023-09-10 16:03:10 -0300

    OrderItem, many-to-many association with extra attributes PART 1

---
+++ src/main/java/com/java_course/webservicesspringbootjpahibernate/entities/pk/OrderItemPK.java
@@ -0,0 +1,55 @@
+package com.java_course.webservicesspringbootjpahibernate.entities.pk;
+
+import com.java_course.webservicesspringbootjpahibernate.entities.Order;
+import com.java_course.webservicesspringbootjpahibernate.entities.Product;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class OrderItemPK implements Serializable {
+    private static final long serialVersionUID = -9277485238L;
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public Order getOrder() {
+        return this.order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return this.product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof OrderItemPK)) {
+            return false;
+        }
+        OrderItemPK orderItemPK = (OrderItemPK) o;
+        return Objects.equals(order, orderItemPK.order) && Objects.equals(product, orderItemPK.product);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(order, product);
+    }
+
+}
