c15f4e29e83602b08f0812aae5d45c3f9e97a629
michael-s-b <michelsilva098@gmail.com>
2023-09-13 10:03:37 -0300

    Exception handling - update

--- src/main/java/com/java_course/webservicesspringbootjpahibernate/services/UserService.java
+++ src/main/java/com/java_course/webservicesspringbootjpahibernate/services/UserService.java
@@ -13,6 +13,8 @@ import com.java_course.webservicesspringbootjpahibernate.repositories.UserReposi
 import com.java_course.webservicesspringbootjpahibernate.services.exceptions.DatabaseException;
 import com.java_course.webservicesspringbootjpahibernate.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
     @Autowired
@@ -44,9 +46,14 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+
     }
 
     private void updateData(User entity, User obj) {
